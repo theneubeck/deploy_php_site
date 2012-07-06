79ded56f8121e6eaf11031f6051af08aaa30dbc1
Jens Neubeck <jens@neubeck.se>
2012-07-06 10:35:14 +0200

    Fixed typo in sql

--- templates/create_db.sql
+++ templates/create_db.sql
@@ -2,4 +2,4 @@ set names utf8;
 CREATE DATABASE %{db_name};
 CREATE USER '%{db_user}'@'localhost' IDENTIFIED BY '%{db_password}';
 GRANT ALL ON %{db_name}.* TO '%{db_user}'@'localhost';
-FLUSH PRIVILEGIES;
+FLUSH PRIVILEGES;
